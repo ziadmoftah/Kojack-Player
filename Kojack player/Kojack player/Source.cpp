3768f890b23b16026ea153092c04e80e80351c41
ziadmoftah <43881897+ziadmoftah@users.noreply.github.com>
2019-04-15 14:28:28 +0200

    Add files via upload

--- Kojack player/Kojack player/Source.cpp
+++ Kojack player/Kojack player/Source.cpp
@@ -17,17 +17,16 @@
 #define directory "C:\\Users\\lenovo\\Documents\\GitHub\\Kojack-Player\\Kojack player\\Kojack player\\resources/"
 #define max_numsongs 10000
 using namespace std;
-using namespace sf;
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800 , 600), "Kojack Player" , sf::Style::Default);
-	
-	
-	sf::RectangleShape seperator(sf::Vector2f(800.0f, 10.0f)); 
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Kojack Player", sf::Style::Default);
+
+
+	sf::RectangleShape seperator(sf::Vector2f(800.0f, 10.0f));
 	seperator.setFillColor(sf::Color::Red);
-	seperator.setPosition(0, 500); 
-	
-	sf::RectangleShape play_button(sf::Vector2f(50, 50)); 
+	seperator.setPosition(0, 500);
+
+	sf::RectangleShape play_button(sf::Vector2f(50, 50));
 	play_button.setOrigin(25, 25);
 	play_button.setPosition(400, 550);
 
@@ -35,7 +34,12 @@ int main() {
 	sf::RectangleShape backward_button(sf::Vector2f(50, 50));
 	backward_button.setOrigin(25, 25);
 	backward_button.setPosition(300, 550);
-	
+
+	sf::RectangleShape shuffle_button(sf::Vector2f(25.0f, 25.0f)); 
+	shuffle_button.setOrigin(12.5, 12.5);
+	shuffle_button.setPosition(150, 550); 
+
+
 
 	sf::RectangleShape forwad_button(sf::Vector2f(50, 50));
 	forwad_button.setOrigin(25, 25);
@@ -43,8 +47,8 @@ int main() {
 
 
 	sf::RectangleShape songs_list(sf::Vector2f(200, 50));
-	songs_list.setFillColor(sf::Color::Green);
-	songs_list.setPosition(0, 80); 
+	songs_list.setFillColor(sf::Color::Magenta);
+	songs_list.setPosition(0, 80);
 
 
 	sf::RectangleShape artist_list(sf::Vector2f(200, 50));
@@ -54,37 +58,37 @@ int main() {
 
 	sf::RectangleShape album_list(sf::Vector2f(200, 50));
 	album_list.setFillColor(sf::Color::Blue);
-	album_list.setPosition(400, 80); 
+	album_list.setPosition(400, 80);
 
 
 
 	sf::RectangleShape genre_list(sf::Vector2f(200, 50));
 	genre_list.setFillColor(sf::Color::Cyan);
-	genre_list.setPosition(600, 80); 
+	genre_list.setPosition(600, 80);
 
 
 
 	sf::Font welcome_font;
 	welcome_font.loadFromFile("parkway lush.ttf");
-	sf::Text welcome; 
+	sf::Text welcome;
 	welcome.setString("Welcome to Kojack player");
-	welcome.setFont(welcome_font); 
+	welcome.setFont(welcome_font);
 	welcome.setFillColor(sf::Color::Black);
-	welcome.setPosition(200, 10); 
-	welcome.setCharacterSize(50); 
-	
+	welcome.setPosition(200, 10);
+	welcome.setCharacterSize(50);
 
 
 
-	sf::RectangleShape back_pic(sf::Vector2f(800.0f,600.0f)); 
+
+	sf::RectangleShape back_pic(sf::Vector2f(800.0f, 600.0f));
 	sf::Texture background_texture;
-	background_texture.loadFromFile("untitled1.png"); 
+	background_texture.loadFromFile("untitled1.png");
 	if (!background_texture.loadFromFile("untitled1.png")) {
-		cout << "error while loading the background"; 
+		cout << "error while loading the background";
 	}
-	back_pic.setTexture(&background_texture); 
-	
-	
+	back_pic.setTexture(&background_texture);
+
+
 
 
 
@@ -93,17 +97,20 @@ int main() {
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			switch(event.type) {
-				case sf::Event::Closed :
-					window.close(); 
-					break; 
-				case sf::Event::MouseMoved :
-					cout << "X: " << event.mouseMove.x << "\t Y: " << event.mouseMove.y << endl;
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::MouseMoved:
+				//cout << "X: " << event.mouseMove.x << "\t Y: " << event.mouseMove.y << endl;
+				break;
+
 			}
-			
+
 		}
 		window.clear();
 		window.draw(back_pic); 
+		window.draw(shuffle_button);
 		window.draw(genre_list);
 		window.draw(album_list); 
 		window.draw(songs_list); 
@@ -117,6 +124,6 @@ int main() {
 	}
 
 
-	//system("Pause"); 
+	system("Pause"); 
 	return 0;
 }
